52985ce4b8b586b5f8dd15103d2ad044057f802d
AbielO <49012222+AbielO@users.noreply.github.com>
2019-06-28 20:55:38 -0500

    Add files via upload

--- Flask-HW.py
+++ Flask-HW.py
@@ -1,18 +1,24 @@
 import numpy as np
 
-#import sqlalchemy
-#from sqlalchemy.ext.automap import automap_base
-#from sqlalchemy.orm import Session
-#from sqlalchemy import create_engine, func
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
-#prcp_engine = create_engine("")
+prcp_engine = create_engine("sqlite:///hawaii.sqlite")
 
-#Base = automap_base()
+Base = automap_base()
 
-#Measurement = Base.classes.measurement
-#Station = Base.classes.station
+Base.prepare(prcp_engine, reflect=True)
+
+Base.classes.keys()
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(prcp_engine)
 
 app = Flask(__name__)
 
@@ -28,10 +34,50 @@ def welcome():
         f"/api/v1.0/<start>/<end><br/>"
     )
 
-#@app.route("//api/v1.0/precipitation")
-#def prcp():
-     #return jsonify(prcp_engine)
+@app.route("//api/v1.0/precipitation")
+def prcp():
+     prcp_results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date > '2016-08-22').all()
 
+    prcp_totals = []
+    for prcps in prcp_results:
+        prcp_dictionary = {}
+        prcp_dictionary["date"] = Measurement.date
+        prcp_dictionary["tobs"] = Measurement.tobs
+        prcp_totals.append(prcp_dictionary)
+    return jsonify(prcp_totals)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    station_results = session.query(Station.station).all()
+    
+    stations_total = list(np.ravel(station_results))
+    
+    return jsonify(stations_total)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    tobs_results = session.query(Measurement.tobs).filter(Measurement.date > '2016-08-22').all()
+    
+    tobs_total = list(np.ravel(tobs_results))
+    
+    return jsonify(tobs_total)
+
+
+@app.route("/api/v1.0/<start>")
+    def calc_temp1(start_date)
+      start_date = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).all()
+    return jsonify(calc_temp1)
+
+
+@app.route("/api/v1.0/<start>/<end>")
+    def calc_temp2(start_date, end_date)
+      start_date = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+    return jsonify(calc_temp.)
 
 if __name__ == "__main__":
     app.run(debug=True)
